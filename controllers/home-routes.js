9fe9fe0d86f409a1e3579801113168e4df1749ec
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-30 14:50:14 -0800

    added signup/signin redirectors to home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,6 +46,15 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// Routes user to home page if signed up/logged in (redirected to signup, otherwise)
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
 // GET single post by `id` on home page
 router.get('/post/:id', (req, res) => {
     Post.findOne({
