30f28288f00a81bafe3e4526d18c60fa572c27c1
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-30 16:10:15 -0800

    updated delete post script

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,21 @@
+// Delete post by `id` and redirects user to dashboard
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const postId = window.location.toString().split('/') [
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${postId}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+
+}
+
+document.querySelector('pendingbuttonname').addEventListener('click', deleteFormHandler);
